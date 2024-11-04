445c30dfbe68f740c195b9b22ba8f8bf96418709
j0jds <mrlucassilva7342@gmail.com>
2024-11-04 17:39:07 -0300

    Testing closure with counter function

--- Intermediate/testClosure.js
+++ Intermediate/testClosure.js
@@ -8,3 +8,22 @@ function teste() {
 const valor = teste;
 
 console.log(valor()());
+console.log("------------------------- DIVISÃO -------------------------")
+
+// ------------------------------------- //
+
+function counter(num) {
+    let acc = 0;
+
+    return function() {
+        acc = acc + num;
+
+        return acc;
+    }
+}
+
+const incrementOne = counter(1);
+
+console.log(incrementOne());
+console.log(incrementOne());
+console.log(incrementOne());
