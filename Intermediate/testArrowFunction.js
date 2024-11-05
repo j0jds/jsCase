af755675a274332980c412537d108c10be95bc58
j0jds <mrlucassilva7342@gmail.com>
2024-11-05 17:29:56 -0300

    Testing differences between normal function and arrow function

---
+++ Intermediate/testArrowFunction.js
@@ -0,0 +1,15 @@
+// função comum
+
+const dobro = function (x) {
+    return x * 2;
+};
+
+console.log(dobro(5));
+console.log("------------------------- DIVISÃO -------------------------")
+
+// ----------------------------------------------------------- //
+
+// arrow function
+
+const dobro2 = x => x * 2;
+console.log(dobro2(5));
