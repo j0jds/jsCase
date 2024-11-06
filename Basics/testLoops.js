c71ba70572fd657cbde62c2f3556ab15951206c9
j0jds <mrlucassilva7342@gmail.com>
2024-11-06 17:46:05 -0300

    Test with stack overflow

--- Basics/testLoops.js
+++ Basics/testLoops.js
@@ -1,7 +1,24 @@
 // for
 
-for (i = 0; i < 200; i++) {
+for (i = 0; i < 20; i++) {
     console.log(i);
 }
 
+// --------------------------------------------- //
 
+// infinity loop
+
+function segunda() {
+    console.log('xau');
+    for (i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    primeira();
+}
+
+function primeira () {
+    console.log("oi");
+    segunda();
+}
+debugger;
+segunda();
