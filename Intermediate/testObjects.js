badc2c04fa17915c35a4a815cddf07521b35e9d7
j0jds <mrlucassilva7342@gmail.com>
2024-11-12 17:34:44 -0300

    Testing object propertie

---
+++ Intermediate/testObjects.js
@@ -0,0 +1,12 @@
+const pessoa = {
+
+}
+
+pessoa.nome = 'FelipeFelipeFelipe';
+
+console.log(pessoa);
+
+// Retorna: {nome: 'FelipeFelipeFelipe'}
+
+// Isso acontece porque o javascript atribui uma propriedade a um objeto a partir do momento que
+// essa propriedade é valorizada, independente de o objeto ter ela previamente ou não.
