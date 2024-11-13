ca32cce551303f6dc818cf701585500439edd0a9
j0jds <mrlucassilva7342@gmail.com>
2024-11-13 14:26:24 -0300

    Changing values and more

--- Intermediate/testCall&Apply&Bind.js
+++ Intermediate/testCall&Apply&Bind.js
@@ -1,16 +1,16 @@
-const falar = function() {
-    console.log(`Olá, eu me chamo ${this.nome} e tenho ${this.idade} anos.`);
+const falar = function(profissao, salario) {
+    console.log(`Olá, eu me chamo ${this.nome}, tenho ${this.idade} anos, sou ${profissao} e ganho R$ ${salario} por mês.`);
 }
 
 const epilef = {
     nome: 'EpilefEpilefEpilef',
-    idade: '002',
+    idade: '98',
 }
 
 const mari = {
     nome: 'Mari',
-    idade: '001'
+    idade: '74'
 }
 
-falar.call(mari);
-falar.call(epilef);
+falar.call(epilef, 'agiota', '0,10');
+falar.call(mari, 'gari', '548.973,00' );
