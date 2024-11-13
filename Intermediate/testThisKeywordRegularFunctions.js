7d404ef97c6b01c3290f5786cfc19600905f57e1
j0jds <mrlucassilva7342@gmail.com>
2024-11-13 17:49:42 -0300

    Added new function and more comments (wip)

--- Intermediate/testThisKeywordRegularFunctions.js
+++ Intermediate/testThisKeywordRegularFunctions.js
@@ -1,3 +1,13 @@
+/** Com functions regulares nós temos 4 opções de comportamento do "this":
+ * 
+ * 1 - Caso esteja em um método de um objeto, ela vai apontar para esse objeto.
+ * 2 - Caso esteja em uma instância de um objeto, ela vai apontar para essa instância.
+ * 3 - Caso esteja em uma função regular que não está diretamente ligada a um objeto, ela vai apontar para o objeto "window" ou "global" (Node.js).
+ * * - Caso esteja sendo utilizado o "use strict" o "this" fica undefined. É necessário utilizar o "window".
+ * 4 - Caso esteja em uma função que foi chamada via "call()", "apply()" ou "bind()", ela vai apontar para o objeto do primeiro parâmetro dessas funções.*/ 
+
+
+// 1
 const pessoa = {
     nome: 'Epilef',
     falar() {
@@ -7,6 +17,7 @@ const pessoa = {
 
 pessoa.falar();
 
+// 2
 const carro = function(modelo) {
     this.modelo = modelo;
     this.acelerar = function() {
@@ -16,3 +27,11 @@ const carro = function(modelo) {
 
 const c = new carro ('BMW');
 c.acelerar();
+
+// 3
+"use strict"
+function logar() {
+    console.log(this);
+}
+
+logar();
